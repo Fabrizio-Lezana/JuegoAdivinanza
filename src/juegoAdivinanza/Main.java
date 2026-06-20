cab251d7ab1c03ede09a2c1385326bef395c3a7a
Fabrizio Lezana <80845176+Fabrizio-Lezana@users.noreply.github.com>
2026-06-19 23:38:26 -0300

    Update Main.java

--- src/juegoAdivinanza/Main.java
+++ src/juegoAdivinanza/Main.java
@@ -9,6 +9,7 @@ public class Main {
 		int opcion;
 
 		do {
+			// MENU DE OPCIONES
 			System.out.println("\n===== MENÚ PRINCIPAL =====");
 			System.out.println("1. Jugar");
 			System.out.println("2. Mostrar reglas");
@@ -23,11 +24,13 @@ public class Main {
 			opcion = teclado.nextInt();
 			switch (opcion) {
 			case 1:
+				// Creamos un nuevo objeto GestorJuego e inicamos un Juego 
 				GestorJuego juego = new GestorJuego(5);
 				juego.Jugar();
 				break;
 
 			case 2:
+				// Se muestran las reglas del juego
 				System.out.println("\nREGLAS:");
 				System.out.println("- Debes adivinar un número entre 1 y 100.");
 				System.out.println("- Tienes 5 intentos.");
@@ -35,10 +38,12 @@ public class Main {
 				break;
 
 			case 3:
+				// Finaliza el programa
 				System.out.println("\nGracias por jugar.");
 				break;
 
 			default:
+				// Valida que se ingrese una de las 3 opciones
 				System.out.println("\nOpción inválida. Debe ingresar una opción entre 1 y 3");
 			}
 
